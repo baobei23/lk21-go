31de7f1368fb79e282359c99e063385bc7105011
baobei23 <reginaldsaja98@gmail.com>
2025-08-16 20:26:52 +0700

    fix StreamSeries

--- internal/api/handler/stream.go
+++ internal/api/handler/stream.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/baobei23/lk21-go/internal/scraper"
 	"github.com/gorilla/mux"
@@ -41,10 +42,25 @@ func StreamMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func StreamSeries(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	url := fmt.Sprintf("%s/%s", os.Getenv("ND_URL"), id)
+	parts := strings.Split(id, "-")
+	year := parts[len(parts)-1]
+	seriesId := strings.Join(parts[:len(parts)-1], "-")
+
+	query := r.URL.Query()
+	season := query.Get("season")
+	if season == "" {
+		season = "1"
+	}
+	episode := query.Get("episode")
+	if episode == "" {
+		episode = "1"
+	}
+
+	url := fmt.Sprintf("%s/%s-season-%s-episode-%s-%s", os.Getenv("ND_URL"), seriesId, season, episode, year)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
